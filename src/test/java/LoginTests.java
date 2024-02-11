6a31600cf401f86720dc4bb38c1a7a422f62c5dd
Аліна Паїк <alinapaik143@gmail.com>
2024-02-11 22:00:11 +0200

    update test

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -9,12 +9,12 @@ public class LoginTests extends BaseTest {
     private LoginPage loginPage;
     @BeforeClass
     public void setUpBeforeTest() {
-        webDriver.get("https://pokupon.ua/");
         loginPage = new LoginPage(webDriver);
     }
     @Test
     @Description("Check that login is worked with valid credentials")
     public void loginWithValidCredentials() {
+        webDriver.get("https://pokupon.ua/");
         loginPage.login("yzvntqpioqaumwvqey@cazlp.com",
                 "44250414");
         Assert.assertTrue(loginPage.isUserLoggedIn(), "User isn't logged in successfully");
@@ -23,6 +23,7 @@ public class LoginTests extends BaseTest {
     @Test
     @Description("Check that login with invalid credentials displayed error message")
     public void loginWithInvalidCredentials() {
+        webDriver.get("https://pokupon.ua/");
         loginPage.login("yzvntqpioqaumwvqey@cazlp.com", "invalid_password");
 
         String expectedErrorMessage = " Вы указали неверный e-mail или пароль. ";
